2ceff09c61a33152dc365e056b480ab7aabad3d7
Jesse Maxim <jessmaxim303@gmail.com>
2019-07-26 00:42:56 -0600

    Cleaned up nav section spacing

---
+++ main.js
@@ -0,0 +1,10 @@
+// inner text function
+
+var navTaskList = document.querySelector('.nav__task--list');
+var makeNavList = document.querySelector('.nav__button--list');
+
+makeNavList.addEventListener('click', function);
+
+function tempTaskList() {
+	
+}
