04dd4a39bf23175e75146b3d1661ba1aa67cb386
Jesse Maxim <jessmaxim303@gmail.com>
2019-07-26 21:57:52 -0600

    Set up data model and started pulling user info

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
 // inner text function
 
-var navTaskList = document.querySelector('.nav__task--list');
-var makeNavList = document.querySelector('.nav__button--list');
+var navTaskList = document.getElementById('nav__task--list');
+var makeNavList = document.querySelector('.nav__button--task');
+var taskItemInput = document.querySelector('.nav__input--item');
+var taskTempContainer = document.getElementById('task__temp--container');
+var bodyMainSection = document.querySelector('.section__body--main');
+var makeTaskCard = document.querySelector('.nav__button--list');
+var navTitleInput = document.querySelector('.nav__input--title');
+var sectionTaskCard = document.getElementById('section__task--card');
+// var makeCards = document.querySelector('');
 
-makeNavList.addEventListener('click', function);
+// EVENT LISTENER *******************
 
-function tempTaskList() {
-	
+makeNavList.addEventListener("click", tempTaskList);
+makeTaskCard.addEventListener("click", newTaskCards);
+
+
+// FUNCTIONS ************************
+
+function tempTaskList(e) {
+	event.preventDefault();
+  navTaskList.insertAdjacentHTML('afterbegin',
+    `<article id="task__temp--container">
+      	<img id="nav__task--icondelete" src="icons/delete.svg" alt="Delete Task">
+   	  <h4>${taskItemInput.value}</h4>
+    </article>`)
 }
+// data-id = ${idea.id}
+function newTaskCards(e) {
+	console.log('hello');
+	event.preventDefault();
+	bodyMainSection.insertAdjacentHTML('afterbegin',
+    `<container id="task__card--container">
+   	  <article id="section__task--card">
+    		<h3>${navTitleInput.value}</h3>
+    			<article id="taskCardBody">
+   		  <p>${taskItemInput.value}</p>
+    	</article>
+    </container>`)
+}
+
+// <container id="task__card--container">
+// <article id="section__task--card">
+
+
+
+
---
+++ todo-list.js
@@ -0,0 +1,20 @@
+class ToDo {
+	constructor(id, title) {
+		this.id = id
+		this.title = title;
+		this.tasks = [];
+		this.urgent = false;
+	}
+	// ***Functions****
+	saveToStorage() {
+		// add to localStorage
+	}
+	deleteFromStorage() {
+		// remove from local storage
+	}
+	updatedTodo() {
+		// push to array
+	}
+
+// ***Closing brace***
+}
