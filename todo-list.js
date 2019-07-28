64f8b8ea4277bd4ded7190624b826a1afd0f16d0
Jesse Maxim <jessmaxim303@gmail.com>
2019-07-28 08:27:55 -0600

    Update class info to better fit data

--- todo-list.js
+++ todo-list.js
@@ -1,15 +1,17 @@
 class ToDo {
-	constructor(id, title) {
+	constructor(id, title, tasks, urgent) {
 		this.id = id
 		this.title = title;
 		this.tasks = [];
-		this.urgent = false;
+		this.urgent = urgent;
 	}
 	// ***Functions****
-	saveToStorage() {
+	saveToStorage(todos) {
+		localStorage.setItem("todos", JSON.stringify(todos));
 		// add to localStorage
 	}
 	deleteFromStorage() {
+		localStorage.setItem("todos", JSON.stringify(todos));
 		// remove from local storage
 	}
 	updatedTodo() {
